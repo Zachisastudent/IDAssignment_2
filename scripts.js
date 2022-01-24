ec81cf53c1f902c66dc2fe3567f329730e0ce386
Zacharia Aslam <s10204144@connect.np.edu.sg>
2022-01-24 09:25:49 +0800

    added index,css and js files

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/*function getData() {
+    const response = await fetch('https://corona-api.com/countries')
+    const data = await response.json()
+  }
+var data = JSON.parse(this.response)
+if (request.status >= 200 && request.status < 400) {
+data.array.forEach(element => {
+    
+});(data => {
+  // Log each movie's title
+  console.log(data.name)
+})
+} else{
+    console.log('error')
+}
+request.send()*/
